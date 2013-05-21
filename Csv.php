e4ecd41de37c5071f93b5dc85894c3daa17662ec
Derek McLean <derek.mclean@1-2-let.co.uk>
2013-05-21 10:42:58 +0100

    E$dited giving you the option of getting an array or an array object

--- Csv.php
+++ Csv.php
@@ -3,21 +3,30 @@ class Del_Csv
 {
     /**
      * @param string $csv the file pathname
+     * @param bool $object if true, returns array object, otherwise a plain array
      * @return ArrayObject
      * @throws Exception
      */
-    public static function toArray($csv)
+    public static function toArray($csv,$object = true)
     {
+	if($object == false)
+	{
+		$object = 'array()';
+	}
+	else
+	{
+		$object = 'ArrayObject()';
+	}
         if(!file_exists($csv))
         {
             throw new Exception('CSV File not found',404);
         }
-        $array = new ArrayObject();
+        $array = new $object;
         if (($handle = fopen($csv, "r")) !== FALSE) {
-            while (($data = fgetcsv($csv, 1000, ",")) !== FALSE)
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE)
             {
                 $count = count($data);
-                $item = new ArrayObject();
+                $item = new $object;
                 for($x = 1; $x <= $count; $x ++)
                 {
                     $item->append($data[$x]);
